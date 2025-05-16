627796b9c255e24f3281f41ba5328a29cbb1fbcf
haydeniza <izabellyhayden@hotmail.com>
2025-05-16 19:06:37 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'aula-16-05',
+  webDir: 'dist'
+};
+
+export default config;
