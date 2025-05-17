d93c3b657a3fafff018c96f7953fc63015c2d08b
haydeniza <izabellyhayden@hotmail.com>
2025-05-16 21:19:55 -0400

    Salvando o codigo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,25 @@
+import { IonButton, IonInput, IonItem, IonText } from '@ionic/react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { useState } from 'react';
 
 const Home: React.FC = () => {
+
+  const [digite, setDigite] = useState(0);
+  const [resultado, setresultado] = useState<number | null> (null);
+
+  const CalcularArea = () => {
+    
+    setresultado(3.14 *  digite  * digite); 
+  }
+
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>curso de dispositivos Móveis</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +28,15 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <h1>Criando aplicativo IONIC</h1>
+
+         <IonItem>
+        <IonInput onIonChange={ (e) => setDigite(parseFloat(e.detail.value! ))} label="calcular a circunferência de um circulo" placeholder="Digite o Nº"></IonInput>
+        <IonText color="primary">
+        <h1>O valor da areaé: {resultado}</h1>
+      </IonText>
+        <IonButton onClick= {CalcularArea}>Calcular</IonButton>
+      </IonItem>
       </IonContent>
     </IonPage>
   );
